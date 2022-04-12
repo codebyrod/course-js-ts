b2c8e7c00733dd4b6688cf3daac0b09e1c8d67f0
codebyrod <rodrigos.comunica@gmail.com>
2022-04-12 20:49:29 -0300

    class 37

--- aula37/js/script.js
+++ aula37/js/script.js
@@ -1,9 +1,10 @@
-const frutas = ['Pera', 'Maçã', 'Uva']; //array de única dimensão também é chamado de vetor;
+const frutas = ['Pera', 'Maçã', 'Uva']; 
+//array de única dimensão também é chamado de vetor;
 
-/* for (let i = 0; i < frutas.length; i++) {
+for (let i = 0; i < frutas.length; i++) {
     console.log(frutas[i]);
     console.log(i);
-} */
+}
 
 //For in -> lê os índices ou chaves do objeto
 
@@ -13,13 +14,15 @@ const pessoa = {
     idade: 30
 };
 
-//Eu não sabia que dava pra acessar propriedades do objeto assim:
+//Outro modo pra acessar propriedades do objeto assim:
 console.log(pessoa['nome']); // Luiz
 
 for (let key in pessoa){
     console.log(key, pessoa[key]);
-}
+};
 
-/* for (let i in frutas) {
+/* 
+for (let i in frutas) {
     console.log(i);
-} */
+}
+*/
--- aula37/js/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-for (let i = 0; i < frutas.length; i++) {
-    console.log(frutas[i]);
-    console.log(i);
-}
