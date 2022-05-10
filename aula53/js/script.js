36025120ec3fbbc4de54daea5af3553e1b106b2d
codebyrod <rodrigos.comunica@gmail.com>
2022-05-10 20:40:29 -0300

    updating class 53

--- aula53/js/script.js
+++ aula53/js/script.js
@@ -1,103 +1,55 @@
-//RETORNO DA FUNÇÃO
-//retunr 
-//Retorna um valor
-//Termina a função
+const nome = 'Rodrigo';
 
-function soma( a, b) {
-    return a + b;
+function falaNome() {
+    console.log(nome);
 }
 
-//Se a gente vai no console do navegador e chama a função com valores, ela vai retorna algo pra gente
-//soma(2, 5) //7
+falaNome(); //Rodrigo
 
-//Existem casos de funções que não retornam nada, mas que executam coisas internamente. Como é o caso das funções de evento ao clique e etc.
+//Por exemplo, tanto a function, quanto a const nome estão no escopo global. Isso é escopo léxico. A função reconhece tudo o que está dentro dela, nos seus vizinhos. 
 
-/* document.addEventListener('click', function(){
-    document.body.style.backgroundColor = 'orange'
-}) */
+//Quando a gente busca nome dentro da função, e a variavel não está dentro da função, ele vai escalando, busca no pai, se não achar no pai ele busca no pai do pai e assim sucessivamente até o escopo global. Caso ele não ache em nenum desses escopos, ele vai retornar um erro.
 
-// O que vai definir se a gente usa return ou não, é se a gente vai precisar do valor retornado pela função
+//As coisas tendem a complicar mais quando começamos a usar a chamada programação funcional
 
-//EXEMPLO DE UMA FUNÇÃO QUE CRIA PESSOAS
-function criaPessoa(nome, sobrenome) {
-    return { nome, sobrenome };
-    //Essa linha de cima é o mesmo que
-    //return { nome: nome, sobrenome: sobrenome }
+function usaFalaNome() {
+    falaNome();
 }
 
-const p1 = criaPessoa('Luiz', 'Otávio');
+usaFalaNome(); //Rodrigo
 
-//Utilizar a função dá no mesmo que passar o objeto de forma literal, exemplo:
-const p2 = {
-    nome: 'João',
-    sobrenome: 'Oliveira'
-};
+//Exemplo 2 - Imagine que declaremos um nome dentro de falaNome2. A função vai identificar em primeiro o que está dentro dela. Encontrado a variável vai executar.
 
-console.log(p1);
-console.log(p2);
+const nome2 = 'Rodrigo';
 
-//COMPLEXIFICANDO UM POUCO MAIS
-function falaFrase(comeco) {
-    function falaResto(resto){
-        //Estamos usando na função interna o parâmetro da função externa
-        return comeco + ' ' + resto;
-    }
-    //Aqui abaixo não estou executando a função, estou passando ela literalmente
-    return falaResto;
-    //seria o mesmo que fazer isso:
-    /* return function(resto){
-        return resto;
-    } */
+//independente de onde a função for executada ela vai lembrar do escopo dela
+function falaNome2() {
+    const nome = 'João'
+    console.log(nome);
 }
 
-/* const olaMundo = falaFrase('Olá');
-console.log(olaMundo); //[Function: falaResto] */
+falaNome2(); //João
 
-/* console.log(olaMundo); //É uma função. Agora o que usamos para chamar a função? Os parenteses? */
-
-/* console.log(olaMundo()); //Só que essa função tem parâmetros no qual podemos mandar os argumentos. */ 
-
-/* console.log(olaMundo('Mundo!')); */
-
-const fala = falaFrase('Olá');
-const resto = fala('mundo!');
-
-console.log(resto);
-
-// DEMONSTANDO UTILIDADE DISSO
-//Função que duplica, triplica, quadriplica valor
-
-/* function duplica(n) {
-    return n * 2;
-} */
-
-/* function triplica(n) {
-    return n * 3;
-} */
-
-/* function quadriplica(n) {
-    return n * 4;
-} */
-
-/* console.log(duplica(2));
-console.log(triplica(2));
-console.log(quadriplica(2)); */
-
-//Perceba que estamos repetindo as funções, imagine se tivessemos muitas outras função até multiplo de 20 ou mais. Teríamos que refazer 20x?
-
-function criaMultiplicador(multiplicador) {
-    //multiplicador
-    return function (n) {
-        return n * multiplicador
-    }
+function usaFalaNome2() {
+    falaNome2();
 }
 
-const duplica = criaMultiplicador(2);
-const triplica = criaMultiplicador(3);
-const quadriplica = criaMultiplicador(4);
+usaFalaNome2(); //João
 
-console.log(duplica(2));
-console.log(triplica(2));
-console.log(quadriplica(2));
+//Como terceiro exemplo vamos verificar a reação da function falaNome quando setamos uma variavel dentro da function usaFalaNome.
+const nome3 = 'Rodrigo';
 
-//Essa parte deu uma leve bugada, vale a pena procurar outros materiais de suporte para melhor compreensão
+function falaNome3() {
+    console.log(nome);
+}
+
+falaNome3(); //Rodrigo
+
+function usaFalaNome3() {
+    const nome = 'João'
+    falaNome3();
+}
+
+usaFalaNome3(); //Rodrigo
+
+//Como podemos ver a função ignora a declaração de dentro da função onde está sendo executada porque ela lembra do escopo quando foi declarada.
